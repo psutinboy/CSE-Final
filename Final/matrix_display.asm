797f1309b5c0d9c55687fc20c4f4afaf7383172e
Psutinboy <grantledbetter12@gmail.com>
2025-04-11 12:59:53 -0500

    maybe fixed something??????? not really sure

--- Final/matrix_display.asm
+++ Final/matrix_display.asm
@@ -1,12 +1,12 @@
 ;-----------------------------------------------------
-; Matrix Display Support - Displays 1 row based on ADC input
+; Matrix Display Support - Displays rows based on ADC input
 ;-----------------------------------------------------
-; Assumes ADC result is in R24:R25 (from 0–1023)
-; Lights a single row on the 8x8 matrix via shift registers
+; Assumes ADC result is in R24:R25 (from 0â€“1023)
+; Lights rows 0 through N on the 8x8 matrix via shift registers
 ;-----------------------------------------------------
 
 display_row_from_adc:
-	; Map ADC to 0–7 row index
+	; Map ADC to 0â€“7 row index
 	CLR R18
 	MOV R20, R24
 	MOV R21, R25
@@ -26,15 +26,21 @@ adc_row_loop:
 
 adc_row_done:
 	LDI R24, 0x00        ; All columns ON (active LOW)
-	LDI R25, 1
-	CLR R16
-display_shift_loop:
-	CP R16, R22
-	BREQ display_shift_done
-	LSL R25
-	INC R16
-	RJMP display_shift_loop
-display_shift_done:
-	COM R25              ; Invert row (make only 1 LOW)
+	LDI R25, 0           ; Start with all rows OFF
+	
+	; Create a mask with bits 0 through R22 set
+	LDI R16, 1           ; Start with bit 0 set
+	LDI R17, 0           ; Counter
+create_mask_loop:
+	CP R17, R22
+	BRGE mask_done       ; If counter >= row number, we're done
+	LSL R16              ; Shift left
+	ORI R16, 1           ; Set the bottom bit
+	INC R17              ; Increment counter
+	RJMP create_mask_loop
+	
+mask_done:
+	MOV R25, R16         ; Move mask to R25
+	COM R25              ; Invert rows (active LOW)
 	CALL write_word
 	RET
